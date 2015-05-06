d132f01635a274cc9f4919ea0abedcbf9c5a1e90
Ryan <hiphubryan@gmail.com>
2015-05-06 12:06:36 +0800

    First commit

---
+++ SimpleChatClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleChatClient
+//
+//  Created by Ryan on 2015/5/6.
+//  Copyright (c) 2015年 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
